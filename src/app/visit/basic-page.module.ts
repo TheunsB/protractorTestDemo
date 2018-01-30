937d428710a489edb8b49ca7b70e445f4f5a70ba
t.breytenbach <t.breytenbach@portbase.com>
2018-01-30 10:11:07 +0100

    Initial commit

---
+++ src/app/visit/basic-page.module.ts
@@ -0,0 +1,20 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BasicPageComponent} from './basic-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    BasicPageComponent
+  ],
+  declarations: [
+    BasicPageComponent
+  ],
+  entryComponents: [
+    BasicPageComponent
+  ]
+})
+export class VisitPaDetailsModule {
+}
