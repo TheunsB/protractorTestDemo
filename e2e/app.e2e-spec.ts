a71165d85521edbc67716e43816a44f48b70f68c
t.breytenbach <t.breytenbach@portbase.com>
2018-01-30 12:57:52 +0100

    added before all refactor step to base demo

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -3,12 +3,12 @@ import { AppPage } from './app.po';
 describe('protractor-test-demo App', () => {
   let page: AppPage;
 
-  beforeEach(() => {
+  beforeAll(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to the Protractor Demo!');
   });
 });
