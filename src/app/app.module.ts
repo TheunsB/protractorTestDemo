98f6f0333c14bff872ba2f7adfb0fc59ccec8215
renier <r.lotter@portbase.com>
2018-01-30 11:08:46 +0100

    Some awesome stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {BasicPageComponent} from './mybasicpage/basic-page.component';
+import {BrowserModule} from '@angular/platform-browser';
+
+const appRoutes: Routes = [
+  { path: 'basic', component: BasicPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mybasicpage/basic-page.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: './basic-page.component.html',
+  styleUrls: ['./basic-page.component.scss']
+})
+export class BasicPageComponent {
+}
